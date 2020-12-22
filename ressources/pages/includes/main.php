13ca203ef24ddc6101b369b4adb403b489c9df16
lain-Dev <55925589+lain-Dev@users.noreply.github.com>
2020-12-22 09:04:54 +0400

    presentation liste de tache sur le fichier main

--- ressources/pages/includes/main.php
+++ ressources/pages/includes/main.php
@@ -5,77 +5,85 @@
 
   <div class="col mb-4 ">
       <div class="card">
-          <div class="card-body bg-primary text-white">
-              <h5 class="card-title">Lundi</h5>
-              <p class="card-text">
-                  This is a longer card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
-              </p>
+          <div class="card-body bg-primary">
+              
+            <ul class="list-group">
+                <li class="list-group-item active ">LUNDI</li>
+                <li class="list-group-item">Dapibus ac facilisis in</li>
+                <li class="list-group-item">Morbi leo risus</li>
+                <li class="list-group-item">Porta ac consectetur ac</li>
+                <li class="list-group-item">Vestibulum at eros</li>
+            </ul>
           </div>
     </div>
   </div>
 
   <div class="col mb-4">
       <div class="card">
-          <div class="card-body bg-secondary text-white">
-              <h5 class="card-title">Mardi</h5>
-              <p class="card-text">
-                  This is a longer card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
-              </p>
+          <div class="card-body bg-secondary">
+          <ul class="list-group">
+                <li class="list-group-item active bg-secondary border-0">MARDI</li>
+                <li class="list-group-item">Dapibus ac facilisis in</li>
+                <li class="list-group-item">Morbi leo risus</li>
+                <li class="list-group-item">Porta ac consectetur ac</li>
+                <li class="list-group-item">Vestibulum at eros</li>
+            </ul>
           </div>
       </div>
   </div>
 
   <div class="col mb-4">
       <div class="card">
-          <div class="card-body bg-success text-white">
-              <h5 class="card-title">Mercredi</h5>
-              <p class="card-text">
-                  This is a longer card with supporting text below as a natural
-                  lead-in to additional content.
-              </p>
+          <div class="card-body bg-success">
+            <ul class="list-group">
+                <li class="list-group-item active bg-success border-0">MERCREDI</li>
+                <li class="list-group-item">Dapibus ac facilisis in</li>
+                <li class="list-group-item">Morbi leo risus</li>
+                <li class="list-group-item">Porta ac consectetur ac</li>
+                <li class="list-group-item">Vestibulum at eros</li>
+            </ul>
           </div>
       </div>
   </div>
 
   <div class="col mb-4">
       <div class="card">
-          <div class="card-body bg-danger text-white">
-          <h5 class="card-title">Jeudi</h5>
-              <p class="card-text">
-                  This is a longer card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
-              </p>
+          <div class="card-body bg-danger">
+            <ul class="list-group">
+                <li class="list-group-item active bg-danger border-0">JEUDI</li>
+                <li class="list-group-item">Dapibus ac facilisis in</li>
+                <li class="list-group-item">Morbi leo risus</li>
+                <li class="list-group-item">Porta ac consectetur ac</li>
+                <li class="list-group-item">Vestibulum at eros</li>
+            </ul>
           </div>
       </div>
   </div>
 
   <div class="col mb-4">
       <div class="card">
-          <div class="card-body bg-warning text-dark">
-              <h5 class="card-title">Vendredi</h5>
-              <p class="card-text">
-                  This is a longer card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
-              </p>
+          <div class="card-body bg-warning">
+            <ul class="list-group">
+                <li class="list-group-item active bg-warning border-0">VENDREDI</li>
+                <li class="list-group-item">Dapibus ac facilisis in</li>
+                <li class="list-group-item">Morbi leo risus</li>
+                <li class="list-group-item">Porta ac consectetur ac</li>
+                <li class="list-group-item">Vestibulum at eros</li>
+            </ul>
           </div>
       </div>
   </div>
 
   <div class="col mb-4">
       <div class="card">
-          <div class="card-body bg-info text-white">
-              <h5 class="card-title">Samedi</h5>
-              <p class="card-text">
-                  This is a longer card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
-              </p>
+          <div class="card-body bg-info">
+            <ul class="list-group">
+                <li class="list-group-item active bg-info border-0">SAMEDI</li>
+                <li class="list-group-item">Dapibus ac facilisis in</li>
+                <li class="list-group-item">Morbi leo risus</li>
+                <li class="list-group-item">Porta ac consectetur ac</li>
+                <li class="list-group-item">Vestibulum at eros</li>
+            </ul>
           </div>
       </div>
       </div>
@@ -83,13 +91,14 @@
 
 <div class="col mb-4">
       <div class="card">
-          <div class="card-body bg-dark text-white">
-              <h5 class="card-title">Dimanche</h5>
-              <p class="card-text">
-              This is a longer card with supporting text below as a natural
-              lead-in to additional content. This content is a little bit
-              longer.
-              </p>
+          <div class="card-body bg-dark">
+            <ul class="list-group">
+                <li class="list-group-item active bg-dark  border-0">DIMANCHE</li>
+                <li class="list-group-item">Dapibus ac facilisis in</li>
+                <li class="list-group-item">Morbi leo risus</li>
+                <li class="list-group-item">Porta ac consectetur ac</li>
+                <li class="list-group-item">Vestibulum at eros</li>
+            </ul>
           </div>
       </div>
   </div>
