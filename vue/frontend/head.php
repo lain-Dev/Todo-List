6b3937cf71c2e67ccd57c37ba5978f7ff96831b9
olivier lebon <olivier@lebon.dev>
2021-03-29 10:22:24 +0400

    suite projet !
    creation fichier head, foot login, home

---
+++ vue/frontend/head.php
@@ -0,0 +1,31 @@
+<?php
+// On démarre une session
+session_start();
+//il faut vérifier si la session est connecté et l'appelé sinon l session n'est pas définis
+if(!isset($_SESSION['user'])){
+    $_SESSION['user'] = false;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!--icon Onglet-->
+    <link rel="shortcut icon" type="image/ico" href="../public/image/icon.png" />
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <!--CSS-->
+    <link rel="stylesheet" href="../public/css/style.css">
+    <!--Font awesome-->
+    <script src="https://use.fontawesome.com/c18e5332f2.js"></script>
+    
+
+    <title>Centre Culturel St-Denis</title>
+
+</head>
+
+<body>
---
+++ vue/home.php
@@ -0,0 +1,5 @@
+<?php require_once(dirname(__DIR__).'/view/frontend/head.php');?>
+
+
+
+<?php require_once(dirname(__DIR__).'/view/footer.php');?>
